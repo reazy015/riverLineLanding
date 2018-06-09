1e6663a1d1e8e0e4e4c85df1d339f857a91f7452
reazy015 <redshuhart015@gmail.com>
2018-06-09 12:06:02 +0300

    preloader fix

--- js/site-preloader.js
+++ js/site-preloader.js
@@ -7,7 +7,7 @@ window.preloader = (function () {
 			document.querySelector('.page').classList.remove('page-loading');
 
 			setTimeout(function() {
-				document.querySelector('.preloader').style.display = 'none';
+				document.querySelector('.preloader').style.display = 'none';				
 			}, 700);
 		}, 1500);
 	}
